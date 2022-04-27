a1a5d9458241dd3bf193db81b05c121d7fb11226
hasiya <hasith002@yahoo.com>
2022-04-27 02:10:18 +0100

    requirements.txt file and reading Encounter Resource data from FHIR files
    Creating a EncounterResource object from the Encounter Resource data from json files.

--- EMIS_FHIR_extract_data/get_resources.py
+++ EMIS_FHIR_extract_data/get_resources.py
@@ -1,6 +1,7 @@
 from os import listdir
 import json
 from fhir.resources.patient import Patient
+from fhir.resources.encounter import Encounter
 
 dir_list = listdir("./data")
 resource_type = set()
@@ -21,6 +22,20 @@ marital_status_dict = {
     "U": "Unmarried",
     "W": "Widowed",
     "UNK": "Unknown"
+}
+
+encounter_class_dict = {
+    "AMB": "Ambulatory",
+    "EMER": "Emergency",
+    "FLD": "Field",
+    "HH": "Home Health",
+    "IMP": "Inpatient Encounter",
+    "ACUTE": "Inpatient Acute",
+    "NONAC": "Inpatient Non-Acute",
+    "OBSENC": "Observation Encounter",
+    "PRENC": "Pre-Admission",
+    "SS": "sShort Stay",
+    "VR": "Virtual"
 }
 
 
@@ -70,7 +85,8 @@ class PatientResource:
         self.marital_status = None
 
     def __str__(self):
-        return f"Name: {self.prefix} {self.given_names} {self.family_name}\n" \
+        return f"id {self.uuid}\n" \
+               f"Name: {self.prefix} {self.given_names} {self.family_name}\n" \
                f"Maiden Name: {self.maiden_name}\n" \
                f"DoB : {self.dob}\n" \
                f"Gender: {self.gender}\n" \
@@ -89,6 +105,28 @@ class EncounterResource:
         self.status = None
         self.subject_uuid = None
         self.encounter_class = None
+        self.encounter_type = None
+        self.primary_performer = None
+        self.start_datetime = None
+        self.end_datetime = None
+        self.discharge_disposition = None
+        self.location = None
+        self.service_provider = None
+        self.reason = None
+
+    def __str__(self):
+        return f"id: {self.uuid}\n" \
+               f"status: {self.status}\n" \
+               f"subject ID: {self.subject_uuid}\n" \
+               f"class: {self.encounter_class}\n" \
+               f"type: {self.encounter_type}\n" \
+               f"primary performer: {self.primary_performer}\n" \
+               f"start: {self.start_datetime}\n" \
+               f"end: {self.end_datetime}\n" \
+               f"discharge: {self.discharge_disposition}\n" \
+               f"location: {self.location}\n" \
+               f"service provider: {self.service_provider}\n" \
+               f"reason: {self.reason}\n"
 
 
 def get_patient_obj(patient_resource):
@@ -138,10 +176,34 @@ def get_patient_obj(patient_resource):
     return patient
 
 
-def read_json(f_name):
-    # this variable is for testing purposes
-    global entry
+def get_encounter_obj(encounter_resource):
+    encounter = EncounterResource()
+    encounter.uuid = encounter_resource.id
+    encounter.status = encounter_resource.status
 
+    subject_ref = encounter_resource.subject.reference
+    encounter.subject_uuid = subject_ref.split(":")[2]
+
+    encounter.encounter_class = encounter_class_dict[encounter_resource.class_fhir.code]
+    encounter.encounter_type = encounter_resource.type[0].text
+    encounter.primary_performer = encounter_resource.participant[0].individual.display
+    encounter.start_datetime = encounter_resource.period.start
+    encounter.end_datetime = encounter_resource.period.end
+
+    if encounter_resource.hospitalization is not None:
+        encounter.discharge_disposition = encounter_resource.hospitalization.dischargeDisposition.text
+
+    encounter.location = encounter_resource.location[0].location.display
+    encounter.service_provider = encounter_resource.serviceProvider.display
+
+    if encounter_resource.reasonCode is not None:
+        encounter.reason = encounter_resource.reasonCode[0].coding[0].display
+
+    return encounter
+
+
+def read_json(f_name):
+    global entry
     # print(f_name)
 
     with open(f_name, 'r') as F:
@@ -168,6 +230,21 @@ def read_json(f_name):
                 # if len(patient.name) > 1:
                 #     print(patient.name[1])
                 #     entry += 1
+            elif res_type == "Encounter":
+                encounter = Encounter.parse_obj(res)
+                encounter_obj = get_encounter_obj(encounter)
+                print(encounter_obj)
+                # if encounter.participant[0].individual.display:
+                if encounter.reasonCode is not None:
+                    # print(encounter.hospitalization)
+                    # if len(encounter.hospitalization.dischargeDisposition.coding) > 0:
+                    #     print(f_name)
+                    #     print(encounter.hospitalization.dischargeDisposition.coding[0].display)
+                    #     print(encounter.hospitalization.dischargeDisposition.coding[0].code)
+                    entry += 1
+            # entry = entry + 1
+
+            resource_type.add(i["resource"]["resourceType"])
 
 
 # print(dir_list)
